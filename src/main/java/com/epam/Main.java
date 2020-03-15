fd0633d8a5a737938377bcbcd2134b182aa3bba5
Azamat Khamzin <azamat_khamzin@epam.com>
2020-03-15 15:36:24 +0300

    remove some stuff

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -1,9 +1,7 @@
 package com.epam;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.tomcat.util.scan.StandardJarScanner;
 
 import java.io.File;
 
@@ -14,12 +12,7 @@ public class Main {
         tomcat.setPort(8080);
         final Connector connector = tomcat.getConnector();
         connector.setURIEncoding("UTF-8");
-        final Context context = tomcat.addWebapp("", new File("src/main/webapp/").getAbsolutePath());
-        StandardJarScanner scan = (StandardJarScanner) context.getJarScanner();
-        scan.setScanClassPath(true);
-        scan.setScanBootstrapClassPath(true); // just guessing here
-        scan.setScanAllDirectories(true);
-        scan.setScanAllFiles(true);
+        tomcat.addWebapp("", new File("src/main/webapp/").getAbsolutePath());
         tomcat.start();
         tomcat.getServer().await();
     }
