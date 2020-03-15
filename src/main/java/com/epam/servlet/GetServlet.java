a24337d0dd916fe2faf225c64e824f43d0427e4c
Azamat Khamzin <azamat_khamzin@epam.com>
2020-03-15 21:06:00 +0300

    add servlet config tast

---
+++ src/main/java/com/epam/servlet/GetServlet.java
@@ -0,0 +1,19 @@
+package com.epam.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final ServletConfig servletConfig = getServletConfig();
+        final String url = servletConfig.getInitParameter("URL");
+        resp.sendRedirect(url);
+    }
+}
